b2322284e8bb57a7f7a71fa1563af4f0b65f03b8
Christopher Collins <chris@spring215.com>
2017-01-27 09:53:56 -0500

    font

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ if (window.location.hostname === 'chrisspring215.github.io') {
     baseTag.setAttribute('href', 'https://chrisspring215.github.io/necto-redesign/')
 }
 else {
-    baseTag.setAttribute('href', 'file:///Users/cacollins/Documents/necto-redesign/index.html')
+    baseTag.setAttribute('href', 'https://chrisspring215.github.io/necto-redesign/')
 }
 
 /** EVENT POPULATION **/

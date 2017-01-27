fc05aca22f489c89102dd462d1e083689667ece5
Christopher Collins <chris@spring215.com>
2017-01-27 09:59:35 -0500

    base tag

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ if (window.location.hostname === 'chrisspring215.github.io') {
     baseTag.setAttribute('href', 'https://chrisspring215.github.io/necto-redesign/')
 }
 else {
-    baseTag.setAttribute('href', 'https://chrisspring215.github.io/necto-redesign/')
+    baseTag.setAttribute('href', 'file:///Users/cacollins/Documents/necto-redesign/')
 }
 
 /** EVENT POPULATION **/

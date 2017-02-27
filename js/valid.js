4b38fcdc2190be834310f23e1f903143b6a5aefb
Christopher Collins <cacollins@cacollins-21616.local>
2017-02-27 12:15:58 -0500

    rental form test

---
+++ js/valid.js
@@ -0,0 +1,17 @@
+function valid()
+{
+	var human =  document.getElementById('human');
+
+	if (human.value == 'yes' | human.value == 'Yes' | human.value == 'YES' | human.value == 'yup')
+		{
+		
+			submit.type = 'submit';
+			document.getElementById("submit").disabled = false;
+			submit.value = "SUBMIT FORM";
+		}
+	else
+		{
+			document.getElementById("submit").disabled = true;
+			submit.value = "PLEASE FILL OUT ALL FIELDS";
+		}
+}
---
+++ renter_contact_from.php
@@ -0,0 +1,15 @@
+<?php
+
+$renter_name = $_POST['renter_name'];
+$renter_phone = $_POST['renter_phone'];
+$renter_email = $_POST['renter_email'];
+$renter_message = $_POST['renter_message'];
+$to = 'vip@necto.com';
+$subject = 'Private Rental';
+
+$message = 'NAME: ' . $renter_name . "\n" . 'PHONE: ' . $renter_phone . "\n" . 'EMAIL: ' . $renter_email . "\n" . 'MESSAGE: ' . $renter_message;
+
+mail ($to, $subject, $message, 'From: ' . $renter_email);
+header("Location: https://www.necto.com/private-event-thank-you.html");
+
+?>

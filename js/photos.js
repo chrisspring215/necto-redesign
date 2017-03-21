3694a477387c02aacba7bafd79e30d701c6c8319
Christopher Collins <cacollins@cacollins-21616.local>
2017-03-21 14:23:54 -0400

    finished building the weekly photos functionality and pages

--- js/photos.js
+++ js/photos.js
@@ -4,19 +4,15 @@ var photosFactory = [
 ];
 
 var photosMix = [
-    "img/weekly-photos/photos/mix" + "https://www.youtube.com/embed/4VOIXPOnfSM?ecver=1",
-    "https://www.youtube.com/embed/X8rlzGmn39I?ecver=1",
-    "https://www.youtube.com/embed/pAl8vVPh6a8?ecver=1",
-    "https://www.youtube.com/embed/pFapnjwvuBk?ecver=1",
-    "https://www.youtube.com/embed/LGajm9WIb10?ecver=1",
+    '1.JPG','12184054_10153079369846230_2589459083081841674_o.jpg','12184323_10153079366691230_7950293210365042243_o.jpg','12186298_10153079363221230_4746671938029978720_o.jpg','12771577_10153249767761230_3358185486170017253_o.jpg','12771619_10153249768006230_399765856042110070_o.jpg','13040974_10153385912696230_8693750457898196689_o.jpg','2.JPG','3.jpg','5.JPG','AFC_8486.jpg','AFC_8502.jpg','AFC_8613.jpg','AFC_8632.jpg','AFC_8649.jpg','AFC_8671.jpg','AFC_8732.jpg','AFC_8747.jpg','AFC_8781.jpg','AFC_8786.jpg','AFC_8923.jpg','AFC_9073.jpg','AFC_9202.jpg','AFC_9232.jpg','IMG_0013.JPG','IMG_0014.JPG','IMG_0015.JPG','IMG_0016.JPG','IMG_0017.JPG','IMG_0037.JPG','IMG_0040.JPG','IMG_0041.JPG','IMG_7321.JPG','IMG_7325.JPG','IMG_7331.JPG','IMG_7332.JPG','IMG_7333.JPG','IMG_7335.JPG','IMG_7385.JPG','IMG_7386.JPG','IMG_7392.JPG','IMG_7393.JPG','IMG_8865.JPG','IMG_8866.JPG','IMG_8867.JPG','IMG_8868.JPG','IMG_8869.JPG','IMG_8871.JPG','IMG_8873.JPG','IMG_8930.JPG','IMG_8931.JPG','IMG_8932.JPG','IMG_8933.JPG','IMG_8934.JPG','IMG_8935.JPG','IMG_8936.JPG','IMG_8937.JPG',
 ];
 
 var photosPride = [
-    "https://www.youtube.com/embed/4VOIXPOnfSM?ecver=1",
+    '1.JPG','IMG_9122.JPG','1520797_10151828632866230_513942927_n.jpg','IMG_9123.JPG','IMG_8875.JPG','IMG_9124.JPG','IMG_8876.JPG','IMG_9125.JPG','IMG_8877.JPG','IMG_9126.JPG','IMG_8878.JPG','IMG_9127.JPG','IMG_8879.JPG','IMG_9128.JPG','IMG_8880.JPG','IMG_9129.JPG','IMG_8882.JPG','IMG_9130.JPG','IMG_8883.JPG','IMG_9131.JPG','IMG_8884.JPG','IMG_9133.JPG','IMG_8885.JPG','IMG_9134.JPG','IMG_8886.JPG','IMG_9135.JPG','IMG_8887.JPG','IMG_9136.JPG','IMG_8888.JPG','IMG_9137.JPG','IMG_8889.JPG','IMG_9138.JPG','IMG_8890.JPG','IMG_9139.JPG','IMG_8891.JPG','IMG_9140.JPG','IMG_9088.JPG','IMG_9141.JPG','IMG_9089.JPG','IMG_9142.JPG','IMG_9091.JPG','IMG_9143.JPG','IMG_9092.JPG','IMG_9144.JPG','IMG_9093.JPG','IMG_9145.JPG','IMG_9094.JPG','IMG_9146.JPG','IMG_9095.JPG','IMG_9147.JPG','IMG_9096.JPG','IMG_9148.JPG','IMG_9097.JPG','IMG_9149.JPG','IMG_9098.JPG','IMG_9150.JPG','IMG_9100.JPG','IMG_9151.JPG','IMG_9101.JPG','IMG_9152.JPG','IMG_9102.JPG','IMG_9153.JPG','IMG_9103.JPG','IMG_9154.JPG','IMG_9104.JPG','IMG_9155.JPG','IMG_9106.JPG','IMG_9156.JPG','IMG_9107.JPG','IMG_9157.JPG','IMG_9108.JPG','IMG_9158.JPG','IMG_9109.JPG','IMG_9159.JPG','IMG_9110.JPG','IMG_9160.JPG','IMG_9111.JPG','IMG_9161.JPG','IMG_9112.JPG','IMG_9168.JPG','IMG_9113.JPG','IMG_9169.JPG','IMG_9114.JPG','IMG_9170.JPG','IMG_9115.JPG','IMG_9171.JPG','IMG_9116.JPG','IMG_9172.JPG','IMG_9117.JPG','IMG_9173.JPG','IMG_9118.JPG','IMG_9174.JPG','IMG_9120.JPG','IMG_9175.JPG','IMG_9121.JPG',
 ];
 
 var photosFrequency = [
-    "https://www.youtube.com/embed/4VOIXPOnfSM?ecver=1",
+    '1.jpg','10494396_10152226446931230_3522937357315790997_o.jpg','10714106_10152350125516230_7594195452783994197_o.jpg','1184933_10151569524061230_1448342399_n.jpg','1234786_10151602006236230_537832335_n.jpg','13002539_10153372945491230_7792885413393377558_o.jpg','1450196_10151696877556230_1249836202_n.jpg','1547993_10152226446881230_5349320145577661395_o.jpg','1617702_10152284310761230_2665471686068889162_o.jpg','1962135_10152385027396230_3865097830303478961_o.jpg','534978_10151359686866230_1073888898_n.jpg','DSC_1528.jpg','DSC_2704.jpg','IMG_8938.JPG','IMG_8939.JPG','IMG_8940.JPG','IMG_8941.JPG','IMG_8942.JPG','IMG_8943.JPG','IMG_8944.JPG','IMG_8945.JPG','IMG_8946.JPG','IMG_8947.JPG','IMG_8948.JPG','IMG_8949.JPG','IMG_8951.JPG','IMG_8952.JPG','IMG_8953.JPG','IMG_8954.JPG','IMG_8955.JPG','IMG_8956.JPG','IMG_8957.JPG','IMG_8958.JPG','IMG_8959.JPG','IMG_8960.JPG','IMG_8961.JPG','IMG_8962.JPG','IMG_8963.JPG','IMG_8964.JPG','IMG_8965.JPG','IMG_8966.JPG','IMG_8967.JPG','IMG_8968.JPG','IMG_8969.JPG','IMG_8970.JPG','IMG_8971.JPG','IMG_8972.JPG','IMG_8973.JPG','IMG_8974.JPG','IMG_8975.JPG','IMG_8976.JPG','IMG_8978.JPG','IMG_8979.JPG','IMG_8980.JPG','IMG_8981.JPG','IMG_8983.JPG','IMG_8984.JPG','IMG_8985.JPG','IMG_8986.JPG','IMG_8987.JPG','IMG_8989.JPG','IMG_8990.JPG','IMG_8991.JPG','IMG_8992.JPG','IMG_8993.JPG','IMG_8994.JPG','IMG_8995.JPG','IMG_8996.JPG','IMG_8997.JPG','IMG_8998.JPG','IMG_8999.JPG','IMG_9001.JPG','IMG_9002.JPG','IMG_9003.JPG','IMG_9004.JPG','IMG_9005.JPG','IMG_9006.JPG','IMG_9007.JPG','IMG_9008.JPG','IMG_9098.JPG','IMG_9101.JPG',
 ];
 
 //Populates videos on the Weekly event pages
@@ -31,10 +27,10 @@ function populateWeeklyPhotos(day) {
     /* fri */ if (day === 5) { numOfPhotos = photosPride.length;     photoPool = photosPride;     photoDayPath = '/photos-pride/'};
     /* sat */ if (day === 6) { numOfPhotos = photosFrequency.length; photoPool = photosFrequency; photoDayPath = '/photos-frequency/'};
 
-    document.getElementById('weekly-photo-1').setAttribute('src', ('img/weekly-photos' + photoDayPath + photoPool[Math.floor(Math.random() * numOfPhotos)].split('.').join('_tn.')));
-    document.getElementById('weekly-photo-2').setAttribute('src', ('img/weekly-photos' + photoDayPath + photoPool[Math.floor(Math.random() * numOfPhotos)].split('.').join('_tn.')));
-    document.getElementById('weekly-photo-3').setAttribute('src', ('img/weekly-photos' + photoDayPath + photoPool[Math.floor(Math.random() * numOfPhotos)].split('.').join('_tn.')));
-    document.getElementById('weekly-photo-4').setAttribute('src', ('img/weekly-photos' + photoDayPath + photoPool[Math.floor(Math.random() * numOfPhotos)].split('.').join('_tn.')));
+    document.getElementById('weekly-photo-1').setAttribute('src', ('img/weekly-photos' + photoDayPath + 'thumbs/' + photoPool[Math.floor(Math.random() * numOfPhotos)].split('.').join('_tn.')));
+    document.getElementById('weekly-photo-2').setAttribute('src', ('img/weekly-photos' + photoDayPath + 'thumbs/' + photoPool[Math.floor(Math.random() * numOfPhotos)].split('.').join('_tn.')));
+    document.getElementById('weekly-photo-3').setAttribute('src', ('img/weekly-photos' + photoDayPath + 'thumbs/' + photoPool[Math.floor(Math.random() * numOfPhotos)].split('.').join('_tn.')));
+    document.getElementById('weekly-photo-4').setAttribute('src', ('img/weekly-photos' + photoDayPath + 'thumbs/' + photoPool[Math.floor(Math.random() * numOfPhotos)].split('.').join('_tn.')));
 
 
     function test() {
@@ -68,7 +64,7 @@ function populateWeeklyPhotosAll(day) {
     /* sat */ if (day === 6) { numOfPhotos = photosFrequency.length; photoPool = photosFrequency; photoDayPath = '/photos-frequency/';  dayAltText = 'Frequesncy Saturday'};
 
     for (var i = 0; i <= numOfPhotos - 1; i++) {
-        weeklyPhotoGrid.innerHTML = weeklyPhotoGrid.innerHTML + ('<div class="col col-6-xs col-2-md"><a href="' + 'img/weekly-photos' + photoDayPath + photoPool[i] + '"><img style="display: block;" src="img/weekly-photos' + photoDayPath + photoPool[i].split('.').join('_tn.') + '" alt="A photo from ' + dayAltText + ' , a weekly event at the Necto Nightclub in Ann Arbor, Michigan."></div>');
+        weeklyPhotoGrid.innerHTML = weeklyPhotoGrid.innerHTML + ('<div class="col col-6-xs col-2-md"><a href="' + 'img/weekly-photos' + photoDayPath + photoPool[i] + '"><img style="display: block;" src="img/weekly-photos' + photoDayPath + 'thumbs/' + photoPool[i].split('.').join('_tn.') + '" alt="A photo from ' + dayAltText + ' , a weekly event at the Necto Nightclub in Ann Arbor, Michigan." title="A photo from ' + dayAltText + ', a weekly event at the Necto Nightclub in Ann Arbor, Michigan."></div>');
     }
 
 }

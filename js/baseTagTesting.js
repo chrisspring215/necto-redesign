8505deb03f4e2fd62b14d3cde6b080f502b2abc3
Christopher Collins <cacollins@cacollins-21616.local>
2017-05-09 12:54:36 -0400

    jksbdvkjs

--- js/baseTagTesting.js
+++ js/baseTagTesting.js
@@ -6,7 +6,7 @@ var baseTag = document.getElementById('baseTag')
 if (window.location.hostname === 'www.necto.com') {
     baseTag.setAttribute('href', 'https://chrisspring215.github.io')
 }
-else if (window.location.hostname === 'www.necto.com') {
+else if (window.location.hostname === 'chrisspring215.github.io') {
     baseTag.setAttribute('href', 'https://chrisspring215.github.io/necto-redesign/')
 }
 else if (window.location.hostname === 'www.necto.com') {

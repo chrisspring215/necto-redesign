3d0d67f1a0c247f2b15b8e32b15b87e12e316c8d
Christopher Collins <cacollins@cacollins-21616.local>
2017-05-09 12:33:29 -0400

    contest

--- js/baseTagTesting.js
+++ js/baseTagTesting.js
@@ -1,11 +1,13 @@
 /* BASE HEAD - FOR TESTING ONLY
  This can be removed or you can add another IFELSE for hostname === necto.com
 */
+
 var baseTag = document.getElementById('baseTag')
 if (window.location.hostname === 'www.necto.com') {
     baseTag.setAttribute('href', 'https://www.necto.com')
 }
+
 else {
-	// baseTag.setAttribute('href', 'file:///Users/cacollins/Documents/necto-redesign/')
-    baseTag.setAttribute('href', 'file:///Users/Spring2/Documents/necto-redesign/') // Switch when working at home or work
+	baseTag.setAttribute('href', 'file:///Users/cacollins/Documents/necto-redesign/')
+    // baseTag.setAttribute('href', 'file:///Users/Spring2/Documents/necto-redesign/') // Switch when working at home or work
 }

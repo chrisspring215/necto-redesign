9565af7431cbaf6ed741098a836f94f2d84bc170
Christopher Collins <cacollins@cacollins-21616.local>
2017-05-18 15:20:40 -0400

    404 update, now points to /?=404restart

--- js/baseTagTesting.js
+++ js/baseTagTesting.js
@@ -4,7 +4,7 @@
 
 var baseTag = document.getElementById('baseTag')
 if (window.location.hostname === 'www.necto.com') {
-    baseTag.setAttribute('href', 'https://chrisspring215.github.io')
+    baseTag.setAttribute('href', 'https://www.necto.com')
 }
 else if (window.location.hostname === 'chrisspring215.github.io') {
     baseTag.setAttribute('href', 'https://chrisspring215.github.io/necto-redesign/')

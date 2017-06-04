4931f9f1312e4d66ee1c101f9c467d5a0b92ae29
Christopher Collins <chris@spring215.com>
2017-06-04 11:27:07 -0400

    qwe

--- js/main.js
+++ js/main.js
@@ -283,12 +283,11 @@ function specialEventPage() {
             now = new Date();
   
             // Adds a newsletter form if the show is more than N hours in the past
-            alert(events[i].eventDate)
-            var newHours = events[i].eventDate.setHours(events[i].eventDate.getHours()+6)
-            alert(newHours)
-            alert(now)
+            var newHours = events[i].eventDate;
+            newHours.setHours(getHours()+4);
 
-            if (events[i].eventDate.getHours()+6 < now.getHours()) {
+
+            if (newHours < now) {
 
                 var mailingMessage = '<div id="mc_embed_signup"><form action="//necto.us11.list-manage.com/subscribe/post?u=07d1e6b4e1063f1729724af7f&amp;id=f126fbdd63" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate><div id="mc_embed_signup_scroll"><h2 class="page-section-sub-header">Did you miss this event?</h2><p>Join our mailing list to stay in the loop on Necto events.</p><div class="mc-field-group"><label for="mce-EMAIL">Email Address </label><input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL"></div><div class="mc-field-group"><label for="mce-FNAME">First Name </label><input type="text" value="" name="FNAME" class="" id="mce-FNAME"></div><div class="mc-field-group"><label for="mce-LNAME">Last Name </label><input type="text" value="" name="LNAME" class="" id="mce-LNAME"></div><div id="mce-responses" class="clear"><div class="response" id="mce-error-response" style="display:none"></div><div class="response" id="mce-success-response" style="display:none"></div></div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--><div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_07d1e6b4e1063f1729724af7f_f126fbdd63" tabindex="-1" value=""></div><div class="clear"><input type="submit" value="SUBSCRIBE" name="subscribe" id="mc-embedded-subscribe" ></div></div></form><br></div>';
 

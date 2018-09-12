1ef537eb6f5f81ea3c4279a8b3123e12f2999f29
Christopher Collins <chris@spring215.com>
2018-09-12 07:39:37 -0400

    Event update

--- js/valid.js
+++ js/valid.js
@@ -2,7 +2,7 @@ function valid()
 {
 	var human =  document.getElementById('human');
 
-	if (human.value == 'yes' | human.value == 'Yes' | human.value == 'YES' | human.value == 'yup')
+	if (human.value === 'yes' | human.value === 'Yes' | human.value === 'YES' | human.value === 'yup' | human.value === 'y' | human.value === 'Y' | human.value === 'Y')
 		{
 		
 			submit.type = 'submit';
